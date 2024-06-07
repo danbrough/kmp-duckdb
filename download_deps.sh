215c32a681ec26af7f4fb772131f789d98eeae71
Dan Brough <dan@mac.local>
2024-06-08 08:36:36 +1200

    update

--- download_deps.sh
+++ download_deps.sh
@@ -7,18 +7,18 @@ cd bin
 
 VERSION=v1.0.0
 
-
 function download_archive() {
     ARCHIVE="$1"
     BINDIR="$2"
     URL="https://github.com/duckdb/duckdb/releases/download/$VERSION/$ARCHIVE"
     ZIP="$BINDIR/$ARCHIVE"
+   echo URL $URL ARCHIVE: $ARCHIVE BINDIR:$BINDIR 
     [ ! -d "$BINDIR" ] && mkdir -p "$BINDIR"
     if [ ! -f "$ZIP" ]; then
         echo downloading $URL to $ZIP
-        curl $URL -o "$ZIP" || exit 1
+        wget $WGETOPTS  $URL -O  "$ZIP" || exit 1
     fi
-    cd "$BINDIR" && unzip $ZIP
+    cd "$BINDIR" && unzip $ARCHIVE && rm $ARCHIVE
 }
 
 MACHINE="$(uname -s)"
@@ -26,8 +26,10 @@ if [ "$MACHINE" == "Linux" ]; then
   download_archive    libduckdb-linux-amd64.zip  amd64
   download_archive    libduckdb-linux-aarch64.zip  aarch64
   download_archive    libduckdb-osx-universal.zip darwin
-elif [ "$MACHINE" == "Mac" ]; then
-  download_archive libduckdb-osx-universal.zip darwin
+elif [ "$MACHINE" == "Darwin" ]; then
+	# dont know why I need this
+	WGETOPTS="--ca-certificate=/etc/ssl/certs/ca-certificates.crt"
+	  download_archive libduckdb-osx-universal.zip darwin
 fi
 
 
