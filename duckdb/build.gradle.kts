910f5517856b5bb94f14cc6fc42257a04ea506cd
Dan Brough <dan@danbrough.org>
2024-06-21 08:35:18 +1200

    update

--- duckdb/build.gradle.kts
+++ duckdb/build.gradle.kts
@@ -16,20 +16,20 @@ import org.jetbrains.kotlin.konan.target.HostManager
 import org.jetbrains.kotlin.konan.target.KonanTarget
 
 plugins {
-  alias(libs.plugins.kotlin.multiplatform)
-  `maven-publish`
-  id("org.danbrough.xtras")
+	alias(libs.plugins.kotlin.multiplatform)
+	`maven-publish`
+	id("org.danbrough.xtras")
 }
 
 
 val KonanTarget.duckdbBinDir: File
-  get() = when (this) {
-    KonanTarget.LINUX_X64 -> file("../bin/amd64")
-    KonanTarget.LINUX_ARM64 -> file("../bin/aarch64")
-    KonanTarget.MINGW_X64 -> file("../bin/windows")
-    KonanTarget.MACOS_X64, KonanTarget.MACOS_ARM64 -> file("../bin/darwin")
-    else -> TODO("Handle target: $this")
-  }
+	get() = when (this) {
+		KonanTarget.LINUX_X64 -> file("../bin/amd64")
+		KonanTarget.LINUX_ARM64 -> file("../bin/aarch64")
+		KonanTarget.MINGW_X64 -> file("../bin/windows")
+		KonanTarget.MACOS_X64, KonanTarget.MACOS_ARM64 -> file("../bin/darwin")
+		else -> TODO("Handle target: $this")
+	}
 
 
 val interopsDefFile = file("src/cinterops/duckdb.def")
@@ -39,91 +39,92 @@ project.generateTypesEnumTask()
 
 
 class Demo(val name: String, val entryPoint: String, vararg args: String) {
-  var description: String = "$name test application"
-  val cmdArgs: Array<out String> = args
+	var description: String = "$name test application"
+	val cmdArgs: Array<out String> = args
 }
 
 val demos = listOf(
-  Demo(
-    "demo1", "org.danbrough.duckdb.demo1",
-    "-d", file("test.db").absolutePath
-  ),
+	Demo(
+		"demo1", "org.danbrough.duckdb.demo1",
+		"-d", file("test.db").absolutePath
+	),
 )
 
 kotlin {
-  jvm()
-  linuxX64()
-  linuxArm64()
-  macosX64()
-  //mingwX64()
+	jvm()
+	linuxX64()
+	linuxArm64()
+	macosX64()
+	//mingwX64()
 
-  @OptIn(ExperimentalKotlinGradlePluginApi::class)
-  compilerOptions {
-    listOf("kotlinx.cinterop.ExperimentalForeignApi").also { optIn = it }
-    freeCompilerArgs = freeCompilerArgs.get() + "-Xexpect-actual-classes"
-  }
-
-
-  val commonMain by sourceSets.getting {
-    dependencies {
-      implementation(libs.klog.core)
-      implementation(libs.clikt)
-    }
-  }
-
-  val commonTest by sourceSets.getting {
-    dependencies {
-      implementation(kotlin("test"))
-      implementation(libs.kotlinx.coroutines)
-      implementation(libs.kotlinx.datetime)
-    }
-  }
-
-  val posixMain by sourceSets.creating {
-    dependsOn(commonMain)
-  }
-
-  val posixTest by sourceSets.creating {
-    dependsOn(commonTest)
-  }
-
-  targets.withType<KotlinNativeTarget> {
-    compilations["main"].apply {
-      defaultSourceSet.dependsOn(posixMain)
-      cinterops {
-        create("duckdb") {
-          definitionFile = interopsDefFile
-          tasks.getByName(interopProcessingTaskName).dependsOn(generateDefFileTaskName)
-        }
-      }
-    }
-
-    compilations["test"].apply {
-      defaultSourceSet.dependsOn(posixTest)
-    }
-
-    binaries {
-      demos.forEach { demoInfo ->
-        executable(demoInfo.name, buildTypes = setOf(NativeBuildType.DEBUG)) {
-          entryPoint = demoInfo.entryPoint
-          compilation = compilations["test"]
-
-          if (konanTarget == HostManager.host)
-            tasks.create("run${demoInfo.name.capitalized()}") {
-              description = demoInfo.description
-              group = "run"
-            }.dependsOn(runTaskName)
+	@OptIn(ExperimentalKotlinGradlePluginApi::class)
+	compilerOptions {
+		listOf("kotlinx.cinterop.ExperimentalForeignApi").also { optIn = it }
+		freeCompilerArgs = freeCompilerArgs.get() + "-Xexpect-actual-classes"
+	}
 
 
-          runTask?.apply {
-            environment(HostManager.host.envLibraryPathName, konanTarget.duckdbBinDir)
-            args(*demoInfo.cmdArgs)
-          }
-        }
-      }
+	val commonMain by sourceSets.getting {
+		dependencies {
+			implementation(libs.klog.core)
+			implementation(libs.clikt)
+			implementation(libs.xtras.support)
+		}
+	}
 
-    }
-  }
+	val commonTest by sourceSets.getting {
+		dependencies {
+			implementation(kotlin("test"))
+			implementation(libs.kotlinx.coroutines)
+			implementation(libs.kotlinx.datetime)
+		}
+	}
+
+	val posixMain by sourceSets.creating {
+		dependsOn(commonMain)
+	}
+
+	val posixTest by sourceSets.creating {
+		dependsOn(commonTest)
+	}
+
+	targets.withType<KotlinNativeTarget> {
+		compilations["main"].apply {
+			defaultSourceSet.dependsOn(posixMain)
+			cinterops {
+				create("duckdb") {
+					definitionFile = interopsDefFile
+					tasks.getByName(interopProcessingTaskName).dependsOn(generateDefFileTaskName)
+				}
+			}
+		}
+
+		compilations["test"].apply {
+			defaultSourceSet.dependsOn(posixTest)
+		}
+
+		binaries {
+			demos.forEach { demoInfo ->
+				executable(demoInfo.name, buildTypes = setOf(NativeBuildType.DEBUG)) {
+					entryPoint = demoInfo.entryPoint
+					compilation = compilations["test"]
+
+					if (konanTarget == HostManager.host)
+						tasks.create("run${demoInfo.name.capitalized()}") {
+							description = demoInfo.description
+							group = "run"
+						}.dependsOn(runTaskName)
+
+
+					runTask?.apply {
+						environment(HostManager.host.envLibraryPathName, konanTarget.duckdbBinDir)
+						args(*demoInfo.cmdArgs)
+					}
+				}
+			}
+
+		}
+	}
 }
 
 
@@ -134,54 +135,54 @@ xtrasTesting {
 xtrasDeclareXtrasRepository()
 
 tasks.register(generateDefFileTaskName) {
-  dependsOn(TASK_GENERATE_TYPES_ENUM)
-  val headersFile = file("src/cinterops/duckdb_headers.def")
-  val codeFile = file("src/cinterops/duckdb_code.h")
-  inputs.files(headersFile, codeFile)
-  outputs.files(interopsDefFile)
+	dependsOn(TASK_GENERATE_TYPES_ENUM)
+	val headersFile = file("src/cinterops/duckdb_headers.def")
+	val codeFile = file("src/cinterops/duckdb_code.h")
+	inputs.files(headersFile, codeFile)
+	outputs.files(interopsDefFile)
 
-  doFirst {
-    kotlin.targets.withType<KotlinNativeTarget>() {
-      val binDir = konanTarget.duckdbBinDir
-      if (!binDir.exists())
-        throw GradleException("$binDir not found. Have you run ./download_deps.sh?")
-    }
-  }
+	doFirst {
+		kotlin.targets.withType<KotlinNativeTarget>() {
+			val binDir = konanTarget.duckdbBinDir
+			if (!binDir.exists())
+				throw GradleException("$binDir not found. Have you run ./download_deps.sh?")
+		}
+	}
 
-  actions.add {
-    headersFile.copyTo(interopsDefFile, overwrite = true)
-  }
+	actions.add {
+		headersFile.copyTo(interopsDefFile, overwrite = true)
+	}
 
-  actions.add {
-    kotlin.targets.withType<KotlinNativeTarget>().forEach { target ->
-      val binDir = target.konanTarget.duckdbBinDir
-      interopsDefFile.appendText(
-        """
+	actions.add {
+		kotlin.targets.withType<KotlinNativeTarget>().forEach { target ->
+			val binDir = target.konanTarget.duckdbBinDir
+			interopsDefFile.appendText(
+				"""
 						linkerOpts.${target.konanTarget.name} = -L${binDir.absolutePath}
 						compilerOpts.${target.konanTarget.name} = -I${binDir.absolutePath}
 						libraryPaths.${target.konanTarget.name} = ${binDir.absolutePath}
 						
 						
 					""".trimIndent()
-      )
-    }
-  }
+			)
+		}
+	}
 
-  actions.add {
-    interopsDefFile.appendText("---\n${codeFile.readText()}\n")
-  }
+	actions.add {
+		interopsDefFile.appendText("---\n${codeFile.readText()}\n")
+	}
 }
 
 
 tasks.withType<Jar> {
-  dependsOn(TASK_GENERATE_TYPES_ENUM)
+	dependsOn(TASK_GENERATE_TYPES_ENUM)
 }
 
 tasks.withType<KotlinCompile> {
-  dependsOn(TASK_GENERATE_TYPES_ENUM)
+	dependsOn(TASK_GENERATE_TYPES_ENUM)
 }
 tasks.withType<KotlinCompileCommon> {
-  dependsOn(TASK_GENERATE_TYPES_ENUM)
+	dependsOn(TASK_GENERATE_TYPES_ENUM)
 }
 
 xtrasDeclareXtrasRepository()
