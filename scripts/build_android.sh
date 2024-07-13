0ac878a89d3a347b2ac0a401405ce5c3452520c7
Dan Brough <dan@danbrough.org>
2024-07-13 15:25:06 +1200

    update

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -5,7 +5,12 @@ cd "$(dirname "$0")" && cd ..
 
 COMMIT=1f98600c2cf8722a6d2f2d805bb4af5e701319fc #v1.0.0
 
-[ -d android-ndk ] && ANDROID_NDK="$PWD/android-ndk"
+if [ -z "$ANDROID_NDK" ]; then
+  echo "\$ANDROID_NDK not set."
+  exit 1
+fi
+
+
 if [ ! -d upstream ]; then
   git clone https://github.com/duckdb/duckdb upstream && \
   cd upstream && git checkout $COMMIT
@@ -18,17 +23,11 @@ if [ $COMMIT != "`git rev-parse HEAD`" ]; then
 fi
 
 
-if [ -z "$ANDROID_NDK" ]; then
-  echo "\$ANDROID_NDK not set."
-  exit 1
-fi
-
-#ANDROID_ABI=arm64-v8a 
-ANDROID_ABI=x86_64
+ANDROID_ABI=arm64-v8a
+#ANDROID_ABI=x86_64
 #ANDROID_ABI=armeabi-v7a
 
 [ ! -z "$1" ] && ANDROID_ABI="$1"
-
 
 ANDROID_PLATFORM=24
 DUCKDB_EXTENSIONS="icu;json;parquet"
@@ -42,7 +41,7 @@ cd $BUILDDIR && \
 cmake -G "Ninja" -DEXTENSION_STATIC_BUILD=1 \
 -DDUCKDB_EXTRA_LINK_FLAGS="-llog" \
 -DBUILD_EXTENSIONS=$DUCKDB_EXTENSIONS \
--DENABLE_EXTENSION_AUTOLOADING=1 -DENABLE_EXTENSION_AUTOINSTALL=1 \
+#-DENABLE_EXTENSION_AUTOLOADING=1 -DENABLE_EXTENSION_AUTOINSTALL=1 \
 -DCMAKE_VERBOSE_MAKEFILE=on \
 -DANDROID_PLATFORM=$ANDROID_PLATFORM \
 -DLOCAL_EXTENSION_REPO=""  -DOVERRIDE_GIT_DESCRIBE="" \
@@ -50,8 +49,10 @@ cmake -G "Ninja" -DEXTENSION_STATIC_BUILD=1 \
 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
 -DCMAKE_BUILD_TYPE=Release ../.. && \
 cmake --build . --config Release
-JNI_LIBDIR=../duckdb/src/androidMain/jniLibs/$ANDROID_ABI
-mkdir -p $JNI_LIBDIR
-cp build/android_$ANDROID_ABI/src/libduckdb.so $JNI_LIBDIR
 
+JNI_LIBDIR=../../../duckdb/src/androidMain/jniLibs/$ANDROID_ABI
+mkdir -p $JNI_LIBDIR
+cp src/libduckdb.so $JNI_LIBDIR
+
+$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-strip $JNI_LIBDIR/libduckdb.so
 
