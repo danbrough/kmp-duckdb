09e4e68b8dd87dfe32535ef3af517821d22a215a
Dan Brough <dan@danbrough.org>
2024-07-28 12:50:24 +1200

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,17 @@
+import com.android.build.api.dsl.Device
+import com.android.build.gradle.internal.setupTaskName
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 
 plugins {
-	alias(libs.plugins.kotlin.multiplatform) apply false
-	alias(libs.plugins.kotlin.android) apply false
-	alias(libs.plugins.org.jetbrains.dokka ) apply false
+  alias(libs.plugins.kotlin.multiplatform) apply false
+  alias(libs.plugins.kotlin.android) apply false
+  alias(libs.plugins.org.jetbrains.dokka) apply false
   alias(libs.plugins.android.application) apply false
-	alias(libs.plugins.android.library) apply false
-	alias(libs.plugins.jetbrains.compose) apply false
-	alias(libs.plugins.compose.compiler) apply false
-	alias(libs.plugins.xtras)
+  alias(libs.plugins.android.library) apply false
+  alias(libs.plugins.jetbrains.compose) apply false
+  alias(libs.plugins.compose.compiler) apply false
+  alias(libs.plugins.xtras)
 }
 
 
-
-tasks.register("thang"){
-	doFirst {
-		println("running gradle task thang.")
-		println("xtras.dir=${findProperty("xtras.dir")}")
-	}
-}
-
-tasks.register("deps"){
-	dependsOn(gradle.includedBuild(rootProject.file("deps").absolutePath).task("build"))
-}
