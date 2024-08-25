52ad66a1297ed8474ce7d3d88c69a03348a25c91
Dan Brough <dan@danbrough.org>
2024-08-25 20:38:20 +1200

    update

--- duckdb/build.gradle.kts
+++ duckdb/build.gradle.kts
@@ -47,15 +47,15 @@ duckdb {
 kotlin {
 
   jvm()
-  linuxX64()
-  linuxArm64()
+
 
   if (HostManager.hostIsMac) {
     macosX64()
     mingwX64()
   } else {
 
-
+    linuxX64()
+    linuxArm64()
     androidNativeX64()
     androidNativeArm64()
     androidNativeArm32()
