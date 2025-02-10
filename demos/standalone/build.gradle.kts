b2d750d5f5514a7ae035fabdea6837184cea75e5
Dan Brough <dan@danbrough.org>
2025-02-09 21:51:51 -0600

    update

---
+++ demos/standalone/build.gradle.kts
@@ -0,0 +1,62 @@
+import org.danbrough.duckdb.duckdb
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.konan.target.HostManager
+
+plugins {
+  alias(libs.plugins.kotlin.multiplatform)
+  alias(libs.plugins.duckdb)
+}
+
+repositories {
+  maven("https://s01.oss.sonatype.org/content/groups/staging/")
+  mavenCentral()
+  gradlePluginPortal()
+}
+
+duckdb {
+  buildEnabled = false
+}
+
+kotlin {
+  applyDefaultHierarchyTemplate()
+
+  linuxX64()
+
+/*
+  if (HostManager.hostIsMac) {
+    macosX64()
+    macosArm64()
+  } else {
+    linuxX64()
+    linuxArm64()
+  }
+*/
+
+
+  val commonMain by sourceSets.getting {
+    dependencies {
+      implementation(libs.klog.core)
+      implementation(libs.kotlinx.coroutines)
+      implementation(libs.xtras.support)
+      implementation(libs.kotlinx.datetime)
+      implementation(libs.xtras.duckdb)
+//      implementation(project(":duckdb"))
+    }
+  }
+
+  val ldLibPathName = if (HostManager.hostIsMac) "DYLD_LIBRARY_PATH" else "LD_LIBRARY_PATH"
+
+  targets.withType<KotlinNativeTarget> {
+    if (this.konanTarget == HostManager.host) {
+
+      binaries {
+        executable("demo1") {
+          entryPoint = "org.danbrough.duckdb.demo1"
+
+          runTask?.environment(ldLibPathName,System.getenv(ldLibPathName))
+        }
+
+      }
+    }
+  }
+}
---
+++ demos/standalone/src/commonMain/kotlin/org/danbrough/duckdb/demo1.kt
@@ -0,0 +1,30 @@
+package org.danbrough.duckdb
+
+import kotlinx.coroutines.runBlocking
+import org.danbrough.xtras.support.getEnv
+
+private val log = klog.logger("DEMO")
+
+fun demo1() {
+  log.info { "demo1" }
+  runBlocking {
+    duckdb(
+      "${getEnv("HOME")}/.habitrack/hb.db",
+      databaseConfig(AccessMode.READ_ONLY)
+    ) {
+
+      connect {
+        query("select {id:event.id,time:event.time,type:enum_code(event.type),count:event.count}::JSON from event") {
+
+          log.trace { "rowCount: $rowCount colCount: $columnCount" }
+
+          for (n in 0 until rowCount) {
+            log.debug { get<String>(n, 0) }
+          }
+        }
+      }
+    }
+  }
+}
+
+
